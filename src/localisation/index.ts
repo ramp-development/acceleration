d4a8e14418d959d6dd3407fa6cd5fad1387018fa
g4knr <85592362+g4knr@users.noreply.github.com>
2023-06-28 13:22:20 +0100

    Updated to only allow default language

--- src/localisation/index.ts
+++ src/localisation/index.ts
@@ -12,18 +12,18 @@ import { applyMarket } from './utils/applyMarket';
 export const localisation = (): void => {
   // Get references to the switcher, market and language elements
   const attr = 'data-localise';
-  const switcherLangs = queryElements<HTMLDivElement>(`[${attr}="switcher-lang"]`);
+  // const switcherLangs = queryElements<HTMLDivElement>(`[${attr}="switcher-lang"]`);
   const switcherIcons = queryElements<HTMLImageElement>(`[${attr}="switcher-icon"]`);
   const marketWrapper = queryElement<HTMLDivElement>(`[${attr}="market-wrapper"]`);
   const marketSelect = queryElement<HTMLSelectElement>(`[${attr}="market-select"]`, marketWrapper);
   const marketList = queryElement<HTMLDivElement>(`[${attr}="market-list"]`);
-  const langWrapper = queryElement<HTMLDivElement>(`[${attr}="lang-wrapper"]`);
-  const langSelect = queryElement<HTMLSelectElement>(`[${attr}="lang-select"]`, langWrapper);
+  // const langWrapper = queryElement<HTMLDivElement>(`[${attr}="lang-wrapper"]`);
+  // const langSelect = queryElement<HTMLSelectElement>(`[${attr}="lang-select"]`, langWrapper);
 
   // Stop execution if necessary elements are not found
-  if (!switcherLangs.length || !switcherIcons.length) return;
+  // if (!switcherLangs.length || !switcherIcons.length) return;
   if (!marketSelect || !marketList) return;
-  if (!langWrapper || !langSelect) return;
+  // if (!langWrapper || !langSelect) return;
 
   // Create a markets array from the links found within the market list
   const marketLinks = queryElements<HTMLAnchorElement>('[data-localise="market-link"]', marketList);
@@ -37,42 +37,42 @@ export const localisation = (): void => {
     })
   );
 
-  // Get all markets that have multiple languages available
-  const multiLanguageMarkets = markets
-    .filter((market) => market.langCodes.length > 1)
-    .map((market) => market.id)
-    .join(', ');
+  // // Get all markets that have multiple languages available
+  // const multiLanguageMarkets = markets
+  //   .filter((market) => market.langCodes.length > 1)
+  //   .map((market) => market.id)
+  //   .join(', ');
 
-  // Set the data attributes for the selector languages
-  switcherLangs.forEach((switcherLang) => {
-    switcherLang.dataset.showIn = multiLanguageMarkets;
-  });
+  // // Set the data attributes for the selector languages
+  // switcherLangs.forEach((switcherLang) => {
+  //   switcherLang.dataset.showIn = multiLanguageMarkets;
+  // });
 
-  // Initialise the languages
-  // Create an array of languages to be added to the dropdown
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  declare const Weglot: any;
-  const weglotLangs: Language[] = [
-    {
-      code: Weglot.options.language_from,
-      name: Weglot.getLanguageName(Weglot.options.language_from),
-    },
-    ...Weglot.options.languages.map((language: { language_to: string }) => {
-      return { code: language.language_to, name: Weglot.getLanguageName(language.language_to) };
-    }),
-  ];
+  // // Initialise the languages
+  // // Create an array of languages to be added to the dropdown
+  // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // declare const Weglot: any;
+  // const weglotLangs: Language[] = [
+  //   {
+  //     code: Weglot.options.language_from,
+  //     name: Weglot.getLanguageName(Weglot.options.language_from),
+  //   },
+  //   ...Weglot.options.languages.map((language: { language_to: string }) => {
+  //     return { code: language.language_to, name: Weglot.getLanguageName(language.language_to) };
+  //   }),
+  // ];
 
-  // Create an array of language options to be added to the dropdown
-  const langOptions = weglotLangs.map(({ code, name }) => new LanguageOption(code, name));
-  langOptions.forEach((langOption) => {
-    langOption.appendTo(langSelect);
-  });
+  // // Create an array of language options to be added to the dropdown
+  // const langOptions = weglotLangs.map(({ code, name }) => new LanguageOption(code, name));
+  // langOptions.forEach((langOption) => {
+  //   langOption.appendTo(langSelect);
+  // });
 
-  // Get the langCode from session storage
-  const langCode = sessionStorage.getItem('langCode') ?? markets[0].langCodes[0];
-  sessionStorage.setItem('langCode', langCode);
-  langSelect.value = langCode;
-  Weglot.switchTo(langCode);
+  // // Get the langCode from session storage
+  // const langCode = sessionStorage.getItem('langCode') ?? markets[0].langCodes[0];
+  // sessionStorage.setItem('langCode', langCode);
+  // langSelect.value = langCode;
+  // Weglot.switchTo(langCode);
 
   // Get all elements that are market-sensitive
   const marketSensitiveElements = queryElements<HTMLDivElement>(
@@ -93,7 +93,7 @@ export const localisation = (): void => {
       if (!market) return;
 
       handleMarketParameter(market.id);
-      handleLangSensitiveElements(market, langOptions, langWrapper);
+      // handleLangSensitiveElements(market, langOptions, langWrapper);
       handleMarketSensitiveElements(market, marketSensitiveElements);
       handleMirrorElements(market.id, mirrorElements);
 
@@ -110,16 +110,16 @@ export const localisation = (): void => {
     // Handling initial market-specific visibility of elements if necessary
     const market = markets.find((market) => market.id === marketSelect.value) as Market;
     if (market === markets[0]) {
-      handleLangSensitiveElements(market, langOptions, langWrapper);
+      // handleLangSensitiveElements(market, langOptions, langWrapper);
       handleMarketSensitiveElements(market, marketSensitiveElements);
     }
   });
 
-  langSelect.addEventListener('change', (event) => {
-    const { value } = event.target as HTMLSelectElement;
-    sessionStorage.setItem('langCode', value);
-    Weglot.switchTo(value);
-  });
+  // langSelect.addEventListener('change', (event) => {
+  //   const { value } = event.target as HTMLSelectElement;
+  //   sessionStorage.setItem('langCode', value);
+  //   Weglot.switchTo(value);
+  // });
 
   // dispatch event once the market select element is ready
   const numberOfMarkets = markets.length;
